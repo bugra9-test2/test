4a0776d2f40a2901985647ea4f56b9327faf0fb3
bugra9-test2 <deneme2@anaoyun.com>
2017-02-07 23:22:05 +0200

    Create bb.js

---
+++ test/bb.js
@@ -0,0 +1,15 @@
+var testClass = new Aaa();
+
+if(chai) {
+    var expect = chai.expect;
+    var assert = chai.assert;
+}
+
+describe('Test...', function(){
+  it('test..', function(){
+    assert(testClass.bb('dd') == "add", 'Error');
+  });
+  it('test2..', function(){
+    assert(testClass.bb('zz') == "zz", 'Error');
+  });
+});
